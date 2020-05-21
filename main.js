e14d1d05fd1c15e5b715dee9d09521a4e9df7dc3
rodrigo_rabelo <rodrigo.rabelo@brunsker.com.br>
2020-05-20 22:26:03 -0300

    20-05-2020

---
+++ main.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector(".btn");
+const form = document.querySelector("form");
+const password = document.querySelector("#password");
+const clientId = document.querySelector("#clientId");
+const connection = document.querySelector("#connection");
+const user = document.querySelector("#user");
+
+btn.addEventListener("click", () => {
+
+  if (!form.checkValidity())
+     alert("Prencha todos os campos corretamente!!");
+  else
+     download();
+
+  clear();
+});
+
+function download() {
+  
+  const credentil = {
+    Username: user.value,
+    Password: password.value,
+    ClientId: parseInt(clientId.value),
+    ConnectionString: connection.value,
+  };
+
+  const json = JSON.stringify(credentil);
+
+  var a = document.createElement("a");
+  a.setAttribute(
+    "href",
+    "data:text/plain;charset=utf-8," + encodeURIComponent(json)
+  );
+  a.setAttribute("download", "UserData.json");
+
+  a.click();
+}
+
+function clear() {
+  user.value = "";
+  password.value = "";
+  clientId.value = "";
+  connection.value = "";
+}
